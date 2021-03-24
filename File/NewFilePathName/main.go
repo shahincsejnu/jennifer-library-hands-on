fd04e685a5fecd0c75db72267ec9a16cb519390d
Sahadat Hossain <shahincsejnu@gmail.com>
2021-03-25 01:20:01 +0600

    added File: NewFilePathName

---
+++ File/NewFilePathName/main.go
@@ -0,0 +1,17 @@
+// NewFilePathName creates a new file with the specified package path and name.
+
+package main
+
+import (
+	"fmt"
+	. "github.com/dave/jennifer/jen"
+)
+
+func main() {
+	f := NewFilePathName("a.b/c", "main")
+	f.Func().Id("main").Params().Block(
+		Qual("a.b/c", "Foo").Call(),
+	)
+
+	fmt.Printf("%#v\n", f)
+}
